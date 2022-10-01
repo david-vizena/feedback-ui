4889c159c7d81c39cbb1cf5097c34886c44929d4
David Vizena <davidvizena@gmail.com>
2022-10-01 10:47:22 -0500

    delete button working

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
-import { useState } from "react"
-import Header from "./components/Header"
-import FeedbackList from "./components/FeedbackList"
-import FeedbackData from "./data/FeedbackData"
+import React from 'react';
+import { useState } from 'react';
+import Header from './components/Header';
+import FeedbackList from './components/FeedbackList';
+import FeedbackData from './data/FeedbackData';
 
 function App() {
-const [feedback, setFeedback] = useState
-(FeedbackData)
+	const [feedback, setFeedback] = useState(FeedbackData);
 
-  return (
-    <>
-    <Header />
-    <div className="container">
-      <FeedbackList feedback={feedback} />
-    </div>
-    </>
-  )
+	const deleteFeedback = (id) => {
+		if (window.confirm('Are you sure you want to delete>')) {
+			setFeedback(feedback.filter((item) => item.id !== id));
+		}
+	};
+
+	return (
+		<>
+			<Header />
+			<div className='container'>
+				<FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+			</div>
+		</>
+	);
 }
 
-export default App
+export default App;
