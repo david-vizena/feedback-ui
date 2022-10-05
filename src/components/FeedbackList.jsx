2a1ee9fa0ef81041abeaa2f4ad6d3ab57cfa06b0
David Vizena <davidvizena@gmail.com>
2022-10-05 15:36:12 -0500

    app complete with static data

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
-import { motion, AnimatePresence} from 'framer-motion'
-import PropTypes from 'prop-types'
+import { motion, AnimatePresence } from 'framer-motion'
+import {useContext} from 'react'
 import FeedbackItem from "./FeedbackItem"
+import FeedbackContext from '../context/FeedbackContext'
 
-function FeedbackList({feedback, handleDelete}) {
+function FeedbackList() {
+  const {feedback} = useContext(FeedbackContext)
+
   console.log(feedback)
 if(!feedback || feedback.length === 0){
 return <p>No Feedback Yet</p>
@@ -22,7 +25,7 @@ return <p>No Feedback Yet</p>
         <FeedbackItem
         key={item.id}
         item={item}
-              handleDelete={handleDelete} />
+               />
           </motion.div>
       ))}
       </AnimatePresence>
@@ -42,14 +45,6 @@ return <p>No Feedback Yet</p>
   // )
 }
 
-FeedbackList.propTypes = {
-feedback: PropTypes.arrayOf(
-  PropTypes.shape({
-    id: PropTypes.number.isRequired,
-    text: PropTypes.string.isRequired,
-    rating: PropTypes.number.isRequired,
-  })
-)
-}
+
 
 export default FeedbackList
--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+import { useContext } from 'react'
+import FeedbackContext from '../context/FeedbackContext'
 
-function FeedbackStats({feedback}) {
+function FeedbackStats() {
+  const { feedback } = useContext(FeedbackContext)
+
 // Calculate ratings avg
 let average = feedback.reduce((acc, cur) =>{
   return acc + cur.rating
@@ -17,8 +20,5 @@ average = average.toFixed(1).replace(/[.,]0$/,'')
   )
 }
 
-FeedbackStats.propTypes = {
-  feedback: PropTypes.array.isRequired,
-}
 
 export default FeedbackStats
